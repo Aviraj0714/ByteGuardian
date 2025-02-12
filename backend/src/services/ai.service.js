bdda6c7b44513babdc63b357fd54e44f2182a09f
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-02-12 21:00:59 +0530

    api intigration

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,12 +1,11 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash",
+    systemInstruction:
+    `you are a code reviewer,who have an expertise in development. you look for the code and find the problems and suggest the solution to the developer. you have to write a review of the code ypu always try to find the best solution for the developer and also try to make the code more efficient and clean .` 
+ });
 
-const prompt = "Explain how AI works";
-
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
 
 
 async function generateContent(prompt) {
